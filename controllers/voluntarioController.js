79a29c52f53ac115e9f008ed4a8050da5c73b80d
Erick S. Gaspar <esgaspar@msn.com>
2017-10-26 14:20:12 -0200

    imagem

--- controllers/voluntarioController.js
+++ controllers/voluntarioController.js
@@ -1,7 +1,57 @@
 "USE STRICT";
-app.controller("voluntarioController", function ($scope, $location, dbService) {
-	// var dataForm = $location.body;
-	// var name = location.files.showFile;
+app.controller("voluntarioController", function ($scope, $location, $compile, dbService) {
+
+	var fs = require('fs');
+
+	$scope.chooseFile = function(name) {
+		var chooser = document.querySelector(name);
+		chooser.addEventListener("change", function (evt) {
+			var el = document.getElementById("showFile");
+			var imageNameInput = document.getElementById("imageName");
+			var emailInput = document.getElementById("email");
+			var img = ["uploads/", new Date().getTime(), ".", this.files[0].name];
+			 var imageNewPath = img[0]+ img[1]+ img[2]+ img[3];
+
+			var source = fs.createReadStream(this.files[0].path);
+			var desti = fs.createWriteStream(img[0] + img[1] + img[2] + img[3]);
+
+			console.log(img[0] + img[1] + img[2] + img[3]);
+
+			el.src = this.files[0].path;
+			imageNameInput.focus();
+			imageNameInput.value = img[0] + img[1] + img[2] + img[3];
+			source.pipe(desti);
+			source.on('end', function () {
+				if(this.files)
+				fs.unlink(this.files[0].path, function (err) {
+					if (err) throw err;
+				});
+			});
+			var buttonAppend = null;
+			var buttonAppend = '<button id=*confirmBtn'+img[1]+'* type=*button* class=*glyphicon glyphicon-ok-circle* ng-click=*changeImage(!' + img[1] + img[2] + img[3] + '!)*/>';
+			
+			buttonAppend = buttonAppend.replace(/\*/g, '"');
+			buttonAppend = buttonAppend.replace(/!/g, '\'');
+			console.log(buttonAppend);
+			
+			var $el = $("#confirmDv button").replaceWith(buttonAppend);
+			var $el = $(buttonAppend).replaceAll("#confirmDv button");
+			$compile($el)($scope);
+			
+			
+		}, false);
+		chooser.click();
+	}
+	
+	function fixedEncodeURIComponent(str) {
+		return encodeURIComponent(str).replace(/[!'()]/g, escape).replace(/\*/g, "%2A");
+	}
+	
+	
+	$scope.changeImage = function (imagePath) {
+		$scope.voluntario.imagem = "uploads/"+imagePath;
+	}
+
 	//Listando
 	$scope.listaVoluntarios = function () {
 		dbService.runAsync("SELECT * FROM voluntarios WHERE ativo = 1", function (data) {
